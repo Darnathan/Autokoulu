63045570bfbe91ca04af81ad50c0374da3c93dd3
Jussi <92643366+Darnathan@users.noreply.github.com>
2023-10-13 21:36:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ const targetIds = ['card1', 'card2', 'card3', 'card4', 'card5', 'card6'];
 const options = {
     root: null,
     rootMargin: '0px',
-    threshold: 0.6, // 60% of the target element is in the viewport
+    threshold: 0.7, // 70% of the target element is in the viewport
 };
 
 const observers = {};
 
-targetIds.forEach((id) => {
-    const target = document.getElementById(id);
+if (window.innerWidth <= 600) {
+    targetIds.forEach((id) => {
+        const target = document.getElementById(id);
 
-    if (target) {
-        observers[id] = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    target.classList.add('in-viewport');
-                } else {
-                    target.classList.remove('in-viewport');
-                }
-            });
-        }, options);
+        if (target) {
+            observers[id] = new IntersectionObserver((entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        target.classList.add('in-viewport');
+                    } else {
+                        target.classList.remove('in-viewport');
+                    }
+                });
+            }, options);
 
-        observers[id].observe(target);
-    }
-});
+            observers[id].observe(target);
+        }
+    });
+}
